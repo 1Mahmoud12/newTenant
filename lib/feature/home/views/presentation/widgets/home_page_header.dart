d53b25d3e4148c5e59e0fb93ded16b4152eebbcd
ah0es <ahmed099essam@gmail>
2025-04-15 20:24:04 +0200

    adding notification to home page

--- lib/feature/home/views/presentation/widgets/home_page_header.dart
+++ lib/feature/home/views/presentation/widgets/home_page_header.dart
@@ -1,3 +1,5 @@
+import 'package:dobzz_seller/core/utils/navigate.dart';
+import 'package:dobzz_seller/feature/notification/presentation/notification_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -7,7 +9,7 @@ class HomePageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(
         children: [
           Column(
@@ -33,23 +35,28 @@ class HomePageHeader extends StatelessWidget {
             ],
           ),
           const Spacer(),
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Icon(Icons.notifications_none, size: 28.sp),
-              Positioned(
-                top: 2,
-                right: 2,
-                child: Container(
-                  width: 8,
-                  height: 8,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
+          InkWell(
+            onTap: () {
+              context.navigateToPage(const NotificationsView());
+            },
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Icon(Icons.notifications_none, size: 28.sp),
+                Positioned(
+                  top: 2,
+                  right: 2,
+                  child: Container(
+                    width: 8,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
