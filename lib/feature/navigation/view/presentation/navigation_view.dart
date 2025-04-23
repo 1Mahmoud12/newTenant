e97542eac3c03e00af1d779d7c72f86931229a49
ah0es <ahmed099essam@gmail>
2025-04-23 15:54:40 +0200

    refactor navigation bar view

--- lib/feature/navigation/view/presentation/navigation_view.dart
+++ lib/feature/navigation/view/presentation/navigation_view.dart
@@ -9,12 +9,14 @@ import 'package:flutter/material.dart';
 class NavigationView extends StatefulWidget {
   const NavigationView({Key? key, this.initialIndex = 0}) : super(key: key);
   final int? initialIndex;
+
   @override
   State<NavigationView> createState() => _NavigationViewState();
 }
 
 class _NavigationViewState extends State<NavigationView> {
   int _selectedIndex = 0;
+
   @override
   void initState() {
     _selectedIndex = widget.initialIndex!;
@@ -27,53 +29,120 @@ class _NavigationViewState extends State<NavigationView> {
     });
   }
 
-  List<Widget> screens = [
+  final List<Widget> _screens = [
     const HomePageView(),
     const CartView(),
     const FavoriteView(),
     const AccountView(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          screens[_selectedIndex],
+          _screens[_selectedIndex],
           Positioned(
             bottom: 5,
             left: 0,
             right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              height: 67,
-              decoration: BoxDecoration(
-                color: const Color(0xff292526),
-                borderRadius: BorderRadius.circular(40),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildNavItem(0, Icons.home_outlined, Icons.home_filled, true),
-                  s,
-                  _buildNavItem(1, Icons.shopping_bag_outlined, Icons.shopping_bag, false),
-                  s,
-                  _buildNavItem(2, Icons.favorite_border, Icons.favorite, false),
-                  s,
-                  _buildNavItem(3, Icons.person_outline, Icons.person, false),
-                ],
-              ),
+            child: NavigationBar(
+              selectedIndex: _selectedIndex,
+              onItemTapped: _onItemTapped,
             ),
           ),
         ],
       ),
     );
   }
+}
 
-  Widget _buildNavItem(int index, IconData outlinedIcon, IconData filledIcon, bool hasNotification) {
-    final isSelected = _selectedIndex == index;
+class NavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const NavigationBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      height: 67,
+      decoration: BoxDecoration(
+        color: const Color(0xff292526),
+        borderRadius: BorderRadius.circular(40),
+      ),
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          NavItem(
+            index: 0,
+            outlinedIcon: Icons.home_outlined,
+            filledIcon: Icons.home_filled,
+            hasNotification: true,
+            isSelected: selectedIndex == 0,
+            onTap: onItemTapped,
+          ),
+          s,
+          NavItem(
+            index: 1,
+            outlinedIcon: Icons.shopping_bag_outlined,
+            filledIcon: Icons.shopping_bag,
+            hasNotification: false,
+            isSelected: selectedIndex == 1,
+            onTap: onItemTapped,
+          ),
+          s,
+          NavItem(
+            index: 2,
+            outlinedIcon: Icons.favorite_border,
+            filledIcon: Icons.favorite,
+            hasNotification: false,
+            isSelected: selectedIndex == 2,
+            onTap: onItemTapped,
+          ),
+          s,
+          NavItem(
+            index: 3,
+            outlinedIcon: Icons.person_outline,
+            filledIcon: Icons.person,
+            hasNotification: false,
+            isSelected: selectedIndex == 3,
+            onTap: onItemTapped,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavItem extends StatelessWidget {
+  final int index;
+  final IconData outlinedIcon;
+  final IconData filledIcon;
+  final bool hasNotification;
+  final bool isSelected;
+  final Function(int) onTap;
+
+  const NavItem({
+    Key? key,
+    required this.index,
+    required this.outlinedIcon,
+    required this.filledIcon,
+    required this.hasNotification,
+    required this.isSelected,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => _onItemTapped(index),
+      onTap: () => onTap(index),
       child: Container(
         padding: const EdgeInsets.all(8),
         width: 65,
@@ -92,7 +161,10 @@ class _NavigationViewState extends State<NavigationView> {
             if (isSelected)
               Container(
                 padding: const EdgeInsets.all(3),
-                decoration: const BoxDecoration(shape: BoxShape.circle, color: AppColors.primaryColor),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.primaryColor,
+                ),
               ),
           ],
         ),
