e311ba6075f53ffe27853d045720fd5f83cff025
mahmoud <mahmoudmdhat852@gmail.com>
2025-05-01 11:33:18 +0300

    update marketing version to 1.0.1, adjust app version to 1.0.2+3, and enable DevicePreview in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:dobzz_seller/feature/splash/view/presentation/splash_screen.dart
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -97,8 +98,8 @@ void main() async {
         startLocale: const Locale('en', 'US'),
         child: DevicePreview(
           // ignore: avoid_redundant_argument_values
-         // enabled: false,
-          enabled: true,
+          // enabled: false,
+          enabled: !kReleaseMode,
           builder: (context) => const MyApp(), // Wrap your app
         ),
       ),
